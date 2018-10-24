77b9b29a625b789a3a27805f9c5e6b4b0c410755
gkourey <grant.kourey@gmail.com>
2018-10-24 11:25:20 -0700

    Updates

---
+++ precache-manifest.d45a1483279106c6abd2d463ab8b9940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/video-search-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e584052b07bdf82fae4e",
+    "url": "/video-search-app/static/js/runtime~main.e584052b.js"
+  },
+  {
+    "revision": "bf645bcd3b2781b88781",
+    "url": "/video-search-app/static/js/main.bf645bcd.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/video-search-app/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "bf645bcd3b2781b88781",
+    "url": "/video-search-app/static/css/main.18d375e5.chunk.css"
+  },
+  {
+    "revision": "64391d21ba7d8f18b8a7c6d6f4b2320e",
+    "url": "/video-search-app/index.html"
+  }
+];
